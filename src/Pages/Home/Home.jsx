dc4c93c8441f6a1c990b3ed52637b4110723c34f
Marcos Paulo M Cabral <69735297+MarcosCabral93@users.noreply.github.com>
2021-09-15 21:25:26 -0300

    feat: este commit adionará a funcionalidade de listagem dos repositorios

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,23 +1,70 @@
-import React from 'react'
-import {FaGithub, FaPlus} from 'react-icons/fa'
-import {Container,Form,BtnSubmit} from "./StyledHome"
-
+import React,{useState, useCallback} from 'react'
+import {FaGithub, FaPlus, FaSpinner, FaBars, FaTrash} from 'react-icons/fa'
+import {Container,Form,BtnSubmit, Lista, Delete} from "./StyledHome"
+import api from '../../Services/Services'
 function Home(){
+    const [novoR, setNovoR]=useState("")
+    const [repositorios, setRepositorios]=useState([])
+    const [espera,setEspera]=useState(false)
+    const handleSubmit=useCallback((e) => {
+        e.preventDefault()
+        setEspera(true)
+        async function sub(){
+                try{
+                const response= await api.get(`repos/${novoR}`)
+                const dados={
+                    nome: response.data.full_name
+                }
+                console.log(dados)
+                setRepositorios([...repositorios, dados])
+                setNovoR("")
+            }       
+            catch(error){
+                console.log(error)
+            }finally{
+            setEspera(false)}
+            }
+            sub()
+        },[novoR,repositorios],
+    )
+    const deletar=useCallback((item)=>{
+
+        const find=repositorios.filter((r)=>r.nome!==item);
+        setRepositorios(find)
+
+    },[repositorios]);
+   
     return (
         <Container>
 
         <h1> <FaGithub size={25}/> Repositorios </h1>
 
         
-        <Form>
-            <input type="text" placeholder="Adicionar Repositorio" />
+        <Form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Adicionar Repositorio" 
+            onChange={e=>setNovoR(e.target.value)} value={novoR} />
 
-            <BtnSubmit>
-                <FaPlus size={15} color='white'/>
+            <BtnSubmit Espera={espera?1:0}>
+                {espera?( <FaSpinner size={15} color ='white'/>):
+                (<FaPlus size={15} color='white'/>)}
             </BtnSubmit>
 
         </Form>
 
+        <Lista>
+            {repositorios.map((item)=>{
+                return(
+                    <li key={item.nome}>
+                        <span>
+                        <Delete onClick={()=> deletar(item.nome)}>
+                            <FaTrash size={14}/>
+                        </Delete>
+                        {item.nome}</span>
+                        <a href="#"> <FaBars size={15}/></a>
+                    </li>
+                )
+            })}
+        </Lista>
 
         </Container>
     )
--- src/Pages/Home/StyledHome.jsx
+++ src/Pages/Home/StyledHome.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, {keyframes,css} from 'styled-components'
 
 export const Container=styled.div`
 margin: 80px auto;
@@ -31,7 +31,20 @@ input{
 
 
 `
-export const BtnSubmit=styled.button`
+
+//animacao
+const animacao=keyframes`
+from{
+    transform: rotate(0deg);
+}
+to{
+    transform: rotate(360deg);
+}
+`
+export const BtnSubmit=styled.button.attrs(props=>({
+type:"submit",
+disabled:props.espera
+}))`
 background-color: #424649;
 border: 0;
 padding: 0 15px;
@@ -40,6 +53,43 @@ border-radius: 5px;
 margin-left: 10px;
 align-items: center;
 justify-content: center;
+
+&[disabled]{
+    cursor:not-allowed;
+    opacity: 0.5;
+}
+${props=> props.espera && 
+css`
+svg{
+    animation: ${animacao} 2s linear infinite;
+}
+`
+}
+`;
+
+export const Lista=styled.ul`
+list-style: none;
+margin-top: 20px;
+
+li{
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+
+    /* so pegar a partir do segundo */
+    & + li{
+        border-top: 1px solid black;
+    }
+    a {
+        color: blue;
+        text-decoration: none;
+    }
+}
+`
+export const Delete=styled.button`
 
 
 `
