2ada58eb2b63cc5eb7580e590b97dda66e8ea2e1
Marcos Paulo M Cabral <69735297+MarcosCabral93@users.noreply.github.com>
2021-09-19 15:59:04 -0300

    feat: este commit adicionará a os items vindos da requisicao

--- src/Pages/Repositorio/Repositorio.jsx
+++ src/Pages/Repositorio/Repositorio.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import {useParams} from 'react-router-dom'
+import {Container, Loading, Card,Btn, Issues} from './Stylerepo'
+import {FaArrowLeft} from 'react-icons/fa'
+import api from '../../Services/Services'
 
-function Repositorio({match}){
+function Repositorio(){
     const {local}=useParams()
+    const p=decodeURIComponent(local)
+    const [rep, setRep]=useState({})
+    const [issues, setIssues]=useState([])
+    const [load, setLoad]= useState(true)
+
+    useEffect(()=>{
+        const req= async ()=>{
+            const [repo , iss]= await Promise.all([
+                api.get(`/repos/${p}`),
+                api.get(`/repos/${p}/issues`,
+                {
+                    params:{
+                        state: 'open',
+                        per_page: 5
+
+                    }
+                })
+
+            ])
+            setRep(repo.data)
+            setIssues(iss.data)
+            setLoad(false)
+            console.log(repo.data)
+            console.log(iss.data)
+        }
+
+       req()
+
+
+
+    },[])
+    if(load){
+        return (
+            <Loading>
+                <h1> Carregando....</h1>
+            </Loading>
+
+        )
+    }
+
+
     return (
-        <h1>{decodeURIComponent(local)}</h1>
+        <Container>
+            <Btn to='/'> 
+            <FaArrowLeft color="black" size={30}/>
+            </Btn>
+            <Card>
+            <h1>{rep.name}</h1>
+            <img src={rep.owner.avatar_url} alt="imagem do repositorio" />
+            <p> {rep.description}</p>
+            </Card>
+            <Issues>
+                
+                {issues.map((item)=>{
+                    return(
+                        <li>
+                        <img src={item.user.avatar_url} alt="imagem do autor da Issue"/>
+
+                        <div>
+                            <strong>
+                                <a href={item.html_url}>{item.title} </a>
+
+                                {item.labels.map((itemL)=>{
+                                    return(
+                                        <span key={String(itemL.id)}>
+                                            {itemL.name}
+
+                                        </span>
+                                    )
+                                })}
+                            </strong>
+                            <p>
+                                {item.user.login}
+                            </p>
+                        </div>
+                        </li>
+                    )
+
+
+                })}
+            </Issues>
+
+        </Container>
+        
     )
 }export default Repositorio
---
+++ src/Pages/Repositorio/Stylerepo.jsx
@@ -0,0 +1,112 @@
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+export const Container=styled.div`
+max-width: 70%;
+background: #fff;
+box-shadow: 0 0 20px rgba(0,0,0,0.2);
+border-radius: 4px;
+padding: 30px;
+margin: 80px auto;
+
+
+`
+
+export const Loading=styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+width: 100%;
+height: 100vh;
+background-color: rgb(214, 213, 213);
+`
+
+export const Card=styled.article`
+display:flex;
+flex-direction: column;
+align-items: center;
+
+img{
+    width: 150px;
+   border-radius: 20%;
+    margin: 20px 0;
+   
+}
+h1{
+    padding: 10px;
+    color: black;
+    font-size: 36px;
+}
+p{
+    padding: 10px;
+    margin-top: 5px;
+    font-size: 13px;
+    text-align: center;
+    line-height: 1.4;
+}
+`
+export const Btn=styled(Link)`
+border: 0;
+outline: 0;
+background: transparent;
+
+padding: 10px;
+`
+export const Issues=styled.ul`
+
+margin-top: 30px;
+padding-top: 30px;
+border-top: 2px solid black;
+list-style: none;
+
+li{
+    display: flex;
+    padding: 15px 10px;
+
+ & + li{
+    margin-top: 15px;
+}
+
+img{
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    border: 2px solid #424649;
+}
+div{
+    display: 1;
+    margin-left: 10px;
+
+    p{
+        margin-top: 10px;
+        font-size: 12px;
+        color:black;
+
+    }
+}
+strong{
+    font-size: 15px;
+
+
+    a{
+        text-decoration: none;
+        color: #424649;
+        transform: 0.5;
+        &:hover{
+
+            color: #0808eb;
+        }
+    }
+    span{
+        background: #222;
+        color: white;
+        border-radius: 4px;
+        font-size: 12px;
+        font-weight: bold;
+        padding: 5px 7px;
+        margin-left: 10px;
+    }
+}
+
+
+}
+`
